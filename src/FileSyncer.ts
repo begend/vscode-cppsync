919370e96174d7d66976465a9f1027413f721426
jackchen0227@sina.com <jackchen0227@sina.com>
2016-11-01 21:41:03 +0800

    ci

--- src/FileSyncer.ts
+++ src/FileSyncer.ts
@@ -1,6 +1,7 @@
 import * as vscode from 'vscode';
 import { window, commands, Disposable, ExtensionContext, TextDocument } from 'vscode';
 import * as fs from 'fs'
+import * as path from 'path'
 
 export default class FileSyncer {
     private _disposable: Disposable;
@@ -18,7 +19,7 @@ export default class FileSyncer {
         var conf = vscode.workspace.getConfiguration('synccpp');
         let srcRootPath: string = conf.get("srcRootPath").toString();
         let destRootPath: string = conf.get("destRootPath").toString();
-        
+
         //let srcRootPath = "d:\\work\\node\\synccode-master"
         //let destRootPath = "d:\\work\\node\\bak"
         this.cpFileImp(doc.fileName, srcRootPath, destRootPath)
@@ -41,9 +42,54 @@ export default class FileSyncer {
             }
         })
 
-        fs.writeFileSync(destFileName, fs.readFileSync(srcFileName));
+        try {
+            fs.writeFileSync(destFileName, fs.readFileSync(srcFileName));
+            vscode.window.showInformationMessage('file sync done!');
+        } catch (error) {
+            vscode.window.showInformationMessage('file sync err!' + error.toString());
+        }
 
         console.log("sync " + srcFileName + " to " + destFileName + " done");
+    }
+
+    cpDir(srcRootPath: string, destRootPath: string) {
+        console.log("src dir " + srcRootPath);
+
+        let files: string[] = fs.readdirSync(srcRootPath);
+
+        files.forEach((file: string) => {
+            let realPath: string = path.join(srcRootPath, file)
+            console.log("in walk " + realPath);
+
+            try {
+                let stat: fs.Stats = fs.statSync(realPath);
+                let destRealPath: string = realPath.replace(srcRootPath, destRootPath);
+                if (stat.isDirectory()) {
+                    console.log("src dir " + realPath);
+                    console.log("dest dir " + destRealPath);
+                    
+                    if (realPath.indexOf(".svn") != -1 
+                        || destRealPath.indexOf(".git") != -1)
+                    {
+                        return;
+                    }
+
+                    if (!fs.existsSync(destRealPath)) {
+                        fs.mkdirSync(destRealPath);
+                    }
+
+                    this.cpDir(realPath, destRealPath);
+                }
+                else {
+                    console.log("src file " + realPath);
+                    console.log("dest file " + destRealPath);
+                    fs.writeFileSync(destRealPath, fs.readFileSync(realPath));
+                }
+            }
+            catch (e) {
+                console.log(e.toString());
+            }
+        });
     }
 
     dispose() {
--- src/extension.ts
+++ src/extension.ts
@@ -12,14 +12,29 @@ export function activate(context: vscode.ExtensionContext) {
     // Use the console to output diagnostic information (console.log) and errors (console.error).
     // This line of code will only be executed once when your extension is activated.
     console.log('Congratulations, your extension "SyncCpp" is now active!');
+    var conf = vscode.workspace.getConfiguration('synccpp');
+    let srcRootPath: string = conf.get("srcRootPath").toString();
+    let destRootPath: string = conf.get("destRootPath").toString();
 
     // create a new word counter
     let fileSyncer = new FileSyncer();
     let controller = new FileSyncController.FileSyncController(fileSyncer);
 
+    let uploadDisp = vscode.commands.registerCommand('extension.uploadDir', () => {
+        fileSyncer.cpDir(srcRootPath, destRootPath);
+        vscode.window.showInformationMessage('uploadDir done!');
+    });
+    
+    let downloadDisp = vscode.commands.registerCommand('extension.downloadDir', () => {
+        fileSyncer.cpDir(destRootPath, srcRootPath);
+        vscode.window.showInformationMessage('downloadDir done');
+    });
+
     // Add to a list of disposables which are disposed when this extension is deactivated.
     context.subscriptions.push(controller);
     context.subscriptions.push(fileSyncer);
+    context.subscriptions.push(uploadDisp);
+    context.subscriptions.push(downloadDisp);
 }
 
 // this method is called when your extension is deactivated
